9b756b18926547d0ab693424ba25336676769e1f
dann7w7 <128494908+dann7w7@users.noreply.github.com>
2023-05-23 14:40:16 +0000

    23 marzo

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import express from 'express';
 import adminRouter from './routes/admin.route.js';
 import shopRouter from './routes/shop.route.js';
 
+import httpStatus from 'http-status';
+
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
@@ -13,9 +15,17 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // Se agrega ruta de administrador
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 // Se agrega ruta shop
 app.use(shopRouter);
+app.use((req, res) =>{
+    res.status(404).send(`
+    <h1 
+    style="color: crimson; text-align: center; font-size: 400%; margin: 3em 0 0 0">
+    404 RESOURCE NOT FOUND 
+    </h1>
+    `);
+});
 
 
 // Definiendo puertos
